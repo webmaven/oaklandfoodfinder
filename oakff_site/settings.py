c251e6ae32af1e44acfd3e0ef344aa84cf065fa7
Michael Bernstein <michael@fandomhome.com>
2011-06-04 16:49:45 -0700

    Initial front page mockup of site

--- oakff_site/settings.py
+++ oakff_site/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_DIR.child('static_root')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,12 +72,10 @@ STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
+# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+        str(PROJECT_DIR.child('static')),
 )
-
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
